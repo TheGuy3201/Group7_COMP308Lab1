425fe95328b414bbf2225a5026046bca5de5cf1c
Joshua Desroches <josh.des.21@gmail.com>
2026-01-26 13:56:46 -0500

    fixed secret leak

--- Group7_COMP308Lab1_Ex2/config/config.js
+++ Group7_COMP308Lab1_Ex2/config/config.js
@@ -2,8 +2,7 @@ const config = {
     env: process.env.NODE_ENV || 'development', 
     port: process.env.PORT || 3000,
     jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key", 
-    mongoUri: process.env.MONGODB_URI ||"mongodb+srv://joshDesro:NTO5tCd0PHKIojuw@assignments.wv4ausy.mongodb.net/assignment1/ex1"||
-    process.env.MONGO_HOST ||
+    mongoUri: process.env.MONGODB_URI ||
     'mongodb://' + (process.env.IP || 'localhost') + ':' + 
    (process.env.MONGO_PORT || '27017') +
     '/assignment1ex2' 
